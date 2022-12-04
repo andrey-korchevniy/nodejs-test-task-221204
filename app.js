bc11fd8d6f8ec4dee5a0aa26eef07bec87b853f3
Andrii Korchevnyi <91483336+andrey-korchevniy@users.noreply.github.com>
2022-12-04 21:07:23 +0200

    check db

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 //9qxgCqiXrlmX0AzS
 const mongoose = require('mongoose'); // MongoDB work library
 
-const DB_HOST =
-  'mongodb+srv://nodetest:9qxgCqiXrlmX0AzS@cluster0.1ampk9j.mongodb.net/users?retryWrites=true&w=majority'; //  'users' DB connection string
-
+const { DB_HOST } = require('./config.');
 // connecting to DB
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log('DB connect'))
+  .then(() => console.log('DB connect !!!!'))
   .catch(error => console.log(error.message));
