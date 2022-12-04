a7a1c446a6b3508a481bc7b6717346306dd01cd4
Andrii Korchevnyi <91483336+andrey-korchevniy@users.noreply.github.com>
2022-12-04 21:50:02 +0200

    hundle db error

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 //9qxgCqiXrlmX0AzS
 const mongoose = require('mongoose'); // MongoDB work library
-const dotenv = require('dotenv'); // import dotenv - npm package for env variables
-
-dotenv.config();
+require('dotenv').config({ path: './.env' }); // import dotenv - npm package for env variables
 
 const { DB_HOST } = process.env;
 // connecting to DB
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log('DB connect !!!!'))
-  .catch(error => console.log(error.message));
+  .then(() => app.listen(3000))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1); // closes all active processes (1 - close undefined errors)
+  });
