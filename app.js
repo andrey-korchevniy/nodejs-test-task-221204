12423109af2a576c72edece81cdd08ba26828ec3
Andrii Korchevnyi <91483336+andrey-korchevniy@users.noreply.github.com>
2022-12-04 22:14:27 +0200

    handle bd error

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-//9qxgCqiXrlmX0AzS
 const mongoose = require('mongoose'); // MongoDB work library
 require('dotenv').config({ path: './.env' }); // import dotenv - npm package for env variables
 
-const { DB_HOST } = process.env;
+const { MONGO_URL } = process.env;
+
+console.log(MONGO_URL);
+
 // connecting to DB
 mongoose
-  .connect(DB_HOST)
-  .then(() => app.listen(3000))
+  .connect(MONGO_URL)
+  .then(() => console.log(555))
   .catch(error => {
     console.log(error.message);
     process.exit(1); // closes all active processes (1 - close undefined errors)
